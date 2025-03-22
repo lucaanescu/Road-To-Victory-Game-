99467fc51b32d32da111d13f6c0dd6d4be8ac07a
Luca Anescu <48320567+lucaanescu@users.noreply.github.com>
2025-03-21 22:28:31 -0400

    eat it science
    game has characters that talk now and a scene that activates

--- road-to-victory/First_map_start.gd
+++ road-to-victory/First_map_start.gd
@@ -6,3 +6,5 @@ extends CanvasLayer
 func _ready():
 	color_Rect.visible = true
 	animated_fadein.play("Fade_out_in")
+	var new_dialog_0 = Dialogic.start('Opening Scene', false)
+	add_child(new_dialog_0)
--- road-to-victory/addons/dialogue_box/dialogue_box.gd
+++
@@ -1,3 +0,0 @@
-extends Node2D
-
-signal Dialogue_box(qote)
