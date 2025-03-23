562cc4a87e778bd7b2720b644221d80d468de1a3
Luca Anescu <48320567+lucaanescu@users.noreply.github.com>
2025-03-22 21:14:49 -0400

    the dialogue starts later
    added a delay till the dialogue can start for dramatic effect
    
    Also some documenting

--- road-to-victory/First_map_start.gd
+++ road-to-victory/First_map_start.gd
@@ -4,7 +4,14 @@ extends CanvasLayer
 @onready var animated_fadein = $AnimationPlayer
 
 func _ready():
+	#opening fadein
 	color_Rect.visible = true
 	animated_fadein.play("Fade_out_in")
+	
+	#timer that starts the dialogue after the opening
+	await get_tree().create_timer(4.2).timeout
+	
+	#Starts the first dialogue of the game
 	var new_dialog_0 = Dialogic.start('Opening Scene', false)
 	add_child(new_dialog_0)
+	
