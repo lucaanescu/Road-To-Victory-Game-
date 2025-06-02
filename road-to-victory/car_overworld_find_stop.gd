d971537cce1757f8b028ae2010c3dcae70a81081
Luca Anescu <48320567+lucaanescu@users.noreply.github.com>
2025-06-02 16:08:31 -0400

    the car still moves
    still need to find out why its not moving to the sprite itself but i think i just need to look into collison 2ds

--- road-to-victory/car_overworld_find_stop.gd
+++ road-to-victory/car_overworld_find_stop.gd
@@ -1,20 +1,23 @@
 extends Node2D
 
-@export var speed : int = 1000
 var Click_event : InputEventMouseButton
 var tween : Tween
 var can_click = true
-
-func _ready() -> void:
-	get_tree().paused = false
+var mouse_position = null
+var current_mouse_pos = null
+var Location_Day_1 : Sprite2D
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("click") && can_click:
 		Click_event = event
-		tween = create_tween()
-		tween.tween_property($Car_overworld,"position",Click_event.position,$Car_overworld.position.distance_to(Click_event.position)/speed)
-		tween.finished.connect(_allow_click)
-		print("Moving")
+		mouse_position = $SanJulioMap.get_global_mouse_position()
+		Location_Day_1 = $EventHolderDay_1/Event1/Sprite2D
+		if mouse_position == Location_Day_1.Collison2D:
+			tween = create_tween()
+			current_mouse_pos = $EventHolderDay_1/Event1/Sprite2D.get_global_mouse_position()
+			tween.tween_property($Car_overworld,"position",current_mouse_pos,1)
+			tween.finished.connect(_allow_click)
+			print("Moving")
 		
 func _allow_click():
 	can_click = true
